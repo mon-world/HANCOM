bc1455ae63512a4f92d68296c1d04ec111471b75
YANG_MON <audgns346712@gmail.com>
2022-06-12 23:56:49 +0900

    220612

--- day34~37/37_02_cost.py
+++ day34~37/37_02_cost.py
@@ -11,4 +11,22 @@ def cost(x, y, w):
 x = [1, 2, 3]
 y = [1, 2, 3]
 
-print(cost(x,y,w=2))
+print(cost(x, y, w=2))
+
+def gradient_descent(x, y, w):
+    c = 0
+    for i in range(len(x)):
+        hx = w * x[i]
+        c += (hx - y[i]) * x[i]
+
+    return c / len(x)
+
+def show_gradient():
+    x = [1, 2, 3]
+    y = [1, 2, 3]
+
+    w = 5
+    for i in range(10):
+        g = gradient_descent(x, y, w)
+        w -= 0.1 * g
+        print(i, w)
